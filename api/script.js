1c2396404aa0e3af2301fb49f29b20b031c3a9ae
leochan <92962941+lc9908@users.noreply.github.com>
2023-01-27 11:42:34 -0500

    Add files via upload

---
+++ api/script.js
@@ -0,0 +1,33 @@
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=21ab7e611e9f17489a04f1ae8663f12c")
+//     .then((response) => response.json())
+//     .then((data) => displayWeather(data));
+
+function getWeather(){
+
+    const search = document.getElementById("search-bar");
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+search.value+"&units=metric&appid=21ab7e611e9f17489a04f1ae8663f12c")
+    .then((response) => response.json())
+    .then((data) => displayWeather(data,search));
+}
+
+function displayWeather(data,search){
+    //element from api
+    if(data.weather != null){
+        document.getElementById("location").innerHTML = data.name;
+        const temperature = Math.round(data.main.temp);
+        const icon = data.weather[0].icon;
+        const humidity = data.main.humidity;
+        const speed = data.wind.speed;
+        const description = data.weather[0].description;
+        document.getElementById("icon").src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+        document.getElementById("temperature").innerHTML = temperature + "°C";
+        document.getElementById("humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.getElementById("description").innerHTML = description;
+        document.getElementById("speed").innerHTML = "Wind Speed: " + speed + "km/h";
+    } else {
+        alert("Location does not exist");
+    }
+
+    search.value ="";
+
+}
